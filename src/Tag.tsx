2c2a666e33155153d1f2398ca8180a8a754afc64
Jean Martineau-Figuette <jean.martineaufiguette@cegedim-activ.com>
2017-12-22 15:16:27 +0100

    improve global tags layout in mobile devices

--- src/Tag.tsx
+++ src/Tag.tsx
@@ -1,24 +1,29 @@
 import * as React from 'react';
 import { ReactChild } from 'react';
-import { css } from 'emotion';
+import styled from 'react-emotion';
 
 interface Props {
     children?: ReactChild;
 }
 
-const tagStyle = css`
+const StyledTag = styled('span')`
     color: white;
     background-color: #50a3d9;
     margin-left: 0.5rem;
     padding: 0.1rem 0.4rem 0.1rem 0.4rem;
     border-radius: 1rem;
     white-space: nowrap;
+    
+    @media screen and (max-width: 1000px) {
+        margin-bottom: 0.3rem;
+    }
+
 `;
 
 const Tag = (props: Props) => (
-    <span className={tagStyle}>
+    <StyledTag>
         {props.children}
-    </span>
+    </StyledTag>
 );
 
 export default Tag;
