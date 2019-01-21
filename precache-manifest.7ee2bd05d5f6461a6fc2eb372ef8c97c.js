d1c216eeac80aaf3454442ca17ba939347e58d8d
euzebe <euzebe@axiome.me>
2019-01-21 04:28:29 +0100

    Updates

--- precache-manifest.7ee2bd05d5f6461a6fc2eb372ef8c97c.js
+++ precache-manifest.7ee2bd05d5f6461a6fc2eb372ef8c97c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/resume/static/js/runtime~main.18bea22a.js"
   },
   {
-    "revision": "3738afda418483f4c5c0",
-    "url": "/resume/static/js/main.3738afda.chunk.js"
+    "revision": "5c710d9e91c3c6aba138",
+    "url": "/resume/static/js/main.5c710d9e.chunk.js"
   },
   {
     "revision": "d66a8a3d04d3a59bbecd",
     "url": "/resume/static/js/1.d66a8a3d.chunk.js"
   },
   {
-    "revision": "3738afda418483f4c5c0",
+    "revision": "5c710d9e91c3c6aba138",
     "url": "/resume/static/css/main.6c798714.chunk.css"
   },
   {
-    "revision": "d7d40b74344151e713f0e5f12fba2557",
+    "revision": "4e4267fb25c61a866bc87e3a32a95518",
     "url": "/resume/index.html"
   }
 ];
